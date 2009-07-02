46ec6b70b82a7e621b1b005ddb9d529069cfd983
Chris Herring <chris.herring.iphone@gmail.com>
2009-07-02 21:45:23 +1000

    Fixing the model to work with first version

--- spec/fixtures/models.rb
+++ spec/fixtures/models.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
 
-  acts_as_rangetastic #:fields => ["fulfilled_on","ordered_on","shipped_on"]
+  acts_as_rangetastic :fields => ["fulfilled_on","ordered_on","shipped_on"]
   
   named_scope :fulfilled, :conditions => ["fulfilled_on is not null"]
   
